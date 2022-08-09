6bf2df0fc97c05380d161c0cb66daf5cee6ed8b6
Alvise <alvise199999@gmail.com>
2022-08-09 11:19:19 +0200

    tabella

--- Admin.php
+++ Admin.php
@@ -7,6 +7,39 @@
   <div class="col-12">
     <h1>Operazioni Admin:</h1>
     <h3>Crea Conferenza:</h3>
+    <table class="table table-dark table-striped">
+        <thead>
+            <tr>
+            <th scope="col">Anno Edizione</th>
+            <th scope="col">Acronimo</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Data Svolgimento</th>
+            <th scope="col">Logo </th>
+            <th scope="col">Bottone </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            </tr>
+            <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            </tr>
+            <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table>
     <div id="Catalogo"><a id="Scopri" href="/NoloNolo/Progetto_TW/NoloNolo/Actions/catalogo.php">Scopri le nostre conferenze</a></div>
   </div>
 
