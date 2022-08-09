d4303c322875f75bb42902bf056f23f260c6d7f0
Alvise <alvise199999@gmail.com>
2022-08-09 09:50:44 +0200

    Seconda

---
+++ Components/footer.php
@@ -0,0 +1,18 @@
+</body>
+<footer class="footer bg-dark">
+      <div class="container" style="width: 70%">
+        <p class="col-lg-12"> 
+          <strong> © Confvirtual. All right reserved </strong> <br>
+           	Developed by  Alvise Amati,  Asperti Andrea,  Fabio Matthias Cejka 
+        </p>
+      </div>
+      <div class="infoMC">
+            <pre>
+              Info e contatti:
+                Via Tripoli 0, Rimini
+                +12 3456789
+                confvirtual@info.com
+            </pre>
+        </div>
+    </footer>
+</html>
