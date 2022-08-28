290e8a2c767f4b3d6415a1dc0a3a726eacf3aefb
Alvise Amati <alvise.amati@studio.unibo.it>
2022-08-28 13:11:01 +0200

    aggiunta file composer mongodb

--- Actions/Authentication/connessioneDbMongo.php
+++ Actions/Authentication/connessioneDbMongo.php
@@ -1,3 +1,7 @@
 <?php
-$manager = new MongoDB\Driver\Manager('mongodb://localhost:27017');
+//scaricato mongo con composer
+require_once 'C:\xampp\htdocs\Progetto_Basi\Confvirtual\vendor\autoload.php';
+$conn = new MongoDB\Client('mongodb://localhost:27017');
+$database = $conn->confvirtual;
+$collection = $database->logs;
 ?>
--- Actions/inserimentoMongoDb.php
+++ Actions/inserimentoMongoDb.php
@@ -1,5 +1,5 @@
 <?php  
-require('../Actions/connessioneDbMongo.php');
+require('../Actions/Authentication/connessioneDbMongo.php');
 
 
 
@@ -11,27 +11,10 @@ $document1 = [
   'prezzo' => '5'
 ]; 
 
-$document2 = [
-  'utente' => 'paolabianchi',  
-  'pizza' => 'diavola',
-  'aggiunta' => 'patatine',  
-  'prezzo' => '7'
-]; 
+$collection->insertOne($document1);
 
-//BulkWrite è la funzionalità per scrivere sul database MongoDB anche più documenti insieme 
-$bulk = new MongoDB\Driver\BulkWrite;  
 
-//preparo i documenti ad una operazione di insert
-$_id1 = $bulk->insert($document1); 
-$_id2 = $bulk->insert($document2); 
 
-//opzionale utile per debug
-//var_dump($_id1, $_id2); 
 
-//connessione MongoDB in localhost
-$manager = new MongoDB\Driver\Manager('mongodb://localhost:27017'); 
-
-//inserisco i documenti nella collection chiamata MyCollection dentro al database chiamato MyDatabase
-$result = $manager->executeBulkWrite('mydatabase.mycollection', $bulk); 
 
 ?> 
