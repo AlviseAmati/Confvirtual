864e82d13e4ac61e1a26440fb4445708bded21a2
Alvise Amati <alvise.amati@studio.unibo.it>
2022-09-12 14:16:41 +0200

    commenti

--- Actions/Authentication/connessioneDbMongo.php
+++ Actions/Authentication/connessioneDbMongo.php
@@ -1,5 +1,5 @@
 <?php
-    //scaricato mongo con composer
+    //scaricato mongo con composer  ,da console entriamo nella root e dopo aver scaricato composer scarichiamo mongodb co comando (se scarichi da github e gia compreso)
     require_once 'C:\xampp\htdocs\Progetto_Basi\Confvirtual\vendor\autoload.php';
     $conn = new MongoDB\Client('mongodb://localhost:27017');
     $db = $conn->confvirtual;
